ca888eb20dcda29237c88a5577852374db881858
Dell-User <Dell-User@DESKTOP-78SJPVI>
2023-10-05 07:21:38 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim LowerCase As String
+        Dim UpperCase As String
+
+        LowerCase = " Visual Basic"
+        UpperCase = "Visual Basic"
+
+        MsgBox(Microsoft.VisualBasic.LCase("Visual Basic"))
+        MsgBox(Microsoft.VisualBasic.UCase("Visual Basic"))
+    End Sub
+End Class
